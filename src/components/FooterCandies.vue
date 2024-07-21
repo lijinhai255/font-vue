ed40d8e7442c26c31514b1105ec9456ec32e5816
ljh255 <jinhai@carbonstop.net>
2024-07-22 07:28:35 +0800

    first commit

---
+++ src/components/FooterCandies.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="fly-footer">
+    <p><a target="_blank">苏苏糖果店铺</a> 2024 &copy;</p>
+    <p>
+      <a
+        href="https://mp.weixin.qq.com/s/wDY7UuQld3U6zub5MUCtbQ"
+        target="_blank"
+        >18615153989</a
+      >
+      <!-- <a
+        href="https://mp.weixin.qq.com/s/H9JI0dR1jPUN12qsRZ2r5Q"
+        target="_blank"
+        >React兴趣小组</a
+      > -->
+      <a
+        href="javascript:void(0)"
+        @mouseover="show()"
+        @mouseout="hide()"
+        style="position:relative;"
+      >
+        微信号（xin9627）
+        <img
+          class="qr"
+          v-if="isShow"
+          src="../assets/img/qrcode-small.jpg"
+          alt
+        />
+      </a>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FooterCandies',
+  data() {
+    return {
+      isShow: false
+    }
+  },
+  methods: {
+    show() {
+      this.isShow = true
+    },
+    hide() {
+      setTimeout(() => {
+        this.isShow = false
+      }, 500)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.qr {
+  width: 120px;
+  height: 120px;
+  position: absolute;
+  left: 0;
+  top: -130px;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <imooc-panel></imooc-panel>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import CandisePanel from '@/components/CandisePanel'
+
+export default {
+  name: 'home',
+  components: {
+    'imooc-panel': CandisePanel
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
